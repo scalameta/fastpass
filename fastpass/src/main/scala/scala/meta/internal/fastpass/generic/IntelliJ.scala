e041a9ad5ad2eec314311d527dda215b1376e980
Myroslav <odisseus@users.noreply.github.com>
2022-08-18 16:28:15 +0200

    Format the libraries.json file
    Currently the JSON is printed as a single long line, which causes rendering problems when the file is opened in IntelliJ.

--- fastpass/src/main/scala/scala/meta/internal/fastpass/generic/IntelliJ.scala
+++ fastpass/src/main/scala/scala/meta/internal/fastpass/generic/IntelliJ.scala
@@ -186,6 +186,6 @@ object IntelliJ {
       case (default, sources) =>
         libraries(default.toString()) = Str(sources.toString())
     }
-    project.root.pantsLibrariesJson.writeText(ujson.write(libraries))
+    project.root.pantsLibrariesJson.writeText(ujson.write(libraries, indent = 4))
   }
 }
